9b9b2ba7b3b5d35861b21449ee4a604a130ee2f5
Artur Diniz Adam <artur.daz@gmail.com>
2018-06-14 19:41:20 -0300

    Arquivos da lib

---
+++ libRelogio.js
@@ -0,0 +1,9 @@
+// tempo não é só uma variável comum
+// tempo é um parâmetro
+function formataRelogio(tempo){
+    var tempoEmHorasFloat = tempo/60
+    var horas = parseInt(tempoEmHorasFloat)
+    var minutos = parseInt((tempoEmHorasFloat - horas) * 60)
+    return horas.toString().padStart(2,0) + ":" + minutos.toString().padStart(2,0)
+
+}
---
+++ mostra.js
@@ -0,0 +1,5 @@
+function mostra(texto){
+    var tag = document.createElement("p")
+    tag.innerHTML = texto
+    document.body.appendChild(tag)
+}
